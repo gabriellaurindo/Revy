db3eee96e5caf7feed63371114248e2bd1e78218
gabriellaurindo <gabrielvitorlaurindo@gmail.com>
2022-05-28 19:55:04 -0300

    Porta e mensagem no server

--- src/server.js
+++ src/server.js
@@ -14,6 +14,6 @@ const server = http.createServer((req, res) => {
   res.end('Hello, World!\n');
 });
 
-server.listen(hostname, () => {
-  console.log('Online');
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);;
 });
